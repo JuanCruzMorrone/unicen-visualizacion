57f222e096db637ec8e57b9dca0d14d9efdbf926
Juan Cruz Morrone <juanchimorrone@gmail.com>
2017-09-07 00:00:01 -0300

    cambios

--- TP1/EntregaFinal/js/main.js
+++ TP1/EntregaFinal/js/main.js
@@ -17,7 +17,9 @@ var imageData;
           var ctx8=c8.getContext("2d");
           var image1 = new Image();
 
-          document.getElementById("file-input").onchange=function addImage(e){
+          document.getElementById("upload").onchange=function addImage(e){
+            document.getElementById("btn-download").style.display="block";
+            document.getElementById("btn-undo").style.display="block";
             var file = e.target.files[0];
             var reader = new FileReader();
             reader.onload = function(e) {
@@ -26,51 +28,39 @@ var imageData;
             reader.readAsDataURL(file);
           };
 
-          // document.getElementById("boton1").onclick=blancoYnegro;
-          // document.getElementById("boton2").onclick=sepia;
-          // document.getElementById("boton3").onclick=invertir;
-          // document.getElementById("boton4").onclick=solarizar;
 
-          
+
           image1.onload = function(){
             dibujar(this);
-            blancoYnegro();
-            sepia();
-            invertir();
-            correccionGamma();
-            sharpear();
-            blurear();
+            blancoYnegro(ctx2,ctx3);
+            sepia(ctx2,ctx4);
+            invertir(ctx2,ctx5);
+            correccionGamma(ctx2,ctx6);
+            sharpear(ctx2,ctx7);
+            blurear(ctx2,ctx8);
           };
           function downloadCanvas(link, canvasId, filename) {
               link.href = document.getElementById(canvasId).toDataURL();
               link.download = filename;
           }
 
-          /** 
-           * The event handler for the link's onclick event. We give THIS as a
-           * parameter (=the link element), ID of the canvas and a filename.
-          */
-          
-          var button = document.getElementById('btn-download');
-            button.addEventListener('click', function (e) {
+          var buttondwn = document.getElementById('btn-download');
+          buttondwn.addEventListener('click', function (e) {
             downloadCanvas(this, 'canvas1', 'test.png');
-            });
-          var canvases =document.getElementsByClassName("canvas");
-          for (var i = 0, len = canvases.length; i < len; i++) {
-            canvases[i].onclick=function changeCanvas(e){
-              var imageData=this.getContext("2d").getImageData(0, 0, image1.width, image1.height);
-              ctx.putImageData(imageData,0,0)
-            }
-          }
-          function myFunction(canvas){
-            canvas.onclick=function changeCanvas(e){
-              var imageData=this.getContext("2d").getImageData(0, 0, image1.width, image1.height);
-              ctx.putImageData(imageData,0,0)
-            }
-          }
+          });
+          var buttonundo = document.getElementById('btn-undo');
+          buttonundo.addEventListener('click', function (e) {
+            ctx.drawImage(image1, 0, 0,canvaswidth,canvasheight);
+          });
 
 
+
+          var canvaswidth=0
+          var canvasheight=0
+          var canvaswidthsml=0
+          var canvasheightsml=0
           function dibujar(image){
+
             var canvas1 =  document.getElementById("canvas1");
             var canvas2 =  document.getElementById("canvas2");
             var canvas3 =  document.getElementById("canvas3");
@@ -79,56 +69,81 @@ var imageData;
             var canvas6 =  document.getElementById("canvas6");
             var canvas7 =  document.getElementById("canvas7");
             var canvas8 =  document.getElementById("canvas8");
-
-            canvas1.width=image.width;
-            canvas1.height=image.height;
+            canvaswidth=700;
+            canvasheight=image.height/(image.width/canvaswidth);
+            canvaswidthsml=canvaswidth/3;
+            canvasheightsml=canvasheight/3;
+            canvas1.width=canvaswidth;
+            canvas1.height=canvasheight;
             canvas1.style.display="default";
-            canvas2.width=(image.width);
-            canvas2.height=(image.height);
+            canvas2.width=canvaswidthsml
+            canvas2.height=canvasheightsml
             canvas2.style.display="default";
-            canvas3.width=(image.width);
-            canvas3.height=(image.height);
+            canvas3.width=canvaswidthsml
+            canvas3.height=canvasheightsml
             canvas3.style.display="default";
-            canvas4.width=(image.width);
-            canvas4.height=(image.height)
+            canvas4.width=canvaswidthsml
+            canvas4.height=canvasheightsml
             canvas4.style.display="default";
-            canvas5.width=(image.width);
-            canvas5.height=(image.height)
+            canvas5.width=canvaswidthsml
+            canvas5.height=canvasheightsml
             canvas5.style.display="default";
-            canvas6.width=(image.width);
-            canvas6.height=(image.height)
+            canvas6.width=canvaswidthsml
+            canvas6.height=canvasheightsml
             canvas6.style.display="default";
-            canvas7.width=(image.width);
-            canvas7.height=(image.height)
+            canvas7.width=canvaswidthsml
+            canvas7.height=canvasheightsml
             canvas7.style.display="default";
-            canvas8.width=(image.width);
-            canvas8.height=(image.height)
+            canvas8.width=canvaswidthsml;
+            canvas8.height=canvasheightsml;
             canvas8.style.display="default";
-            ctx.drawImage(image, 0, 0);
-            ctx2.drawImage(image, 0, 0);
+            document.getElementById("ejemplo-imagenes").style.height=canvasheight+"px";
+            ctx.drawImage(image, 0, 0,canvaswidth,canvasheight);
+            ctx2.drawImage(image, 0, 0,canvaswidthsml,canvasheightsml);
 
-          };
+            canvas2.addEventListener('click', function () {
+              ctx.drawImage(image, 0, 0,canvaswidth,canvasheight);
+            })
+            canvas3.addEventListener('click', function () {
+              blancoYnegro(ctx,ctx);
+            })
+            canvas4.addEventListener('click', function () {
+              sepia(ctx,ctx);
+            })
+            canvas5.addEventListener('click', function () {
+              invertir(ctx,ctx);
+            })
+            canvas6.addEventListener('click', function () {
+              correccionGamma(ctx,ctx);
+            })
+            canvas7.addEventListener('click', function () {
+              sharpear(ctx,ctx);
+            })
+            canvas8.addEventListener('click', function () {
+              blurear(ctx,ctx);
+            })
+
+
+          }
+
 
           function setBrillo(imageData, x, y, brillo){
-            var index=(x+y*image1.width) * 4;
+            var index=(x+y*imageData.width) * 4;
             imageData.data[index+0] = imageData.data[index+0]*(brillo/100);
             imageData.data[index+1] = imageData.data[index+1]*(brillo/100);
             imageData.data[index+2] = imageData.data[index+2]*(brillo/100);
           };
 
-          function correccionGamma(){
+          function correccionGamma(ctxo,ctxn){
             var brillo=150
-            var imageData = ctx2.getImageData(0, 0, image1.width, image1.height);
-
-            for (var i = 0; i < image1.width; i++) {
-              for (var j = 0; j < image1.height; j++) {
+            var imageData = ctxo.getImageData(0, 0, canvaswidth, canvasheight);
+            for (var i = 0; i < imageData.width; i++) {
+              for (var j = 0; j < imageData.height; j++) {
                 setBrillo(imageData, i, j, brillo);
               }
             }
-            ctx6.putImageData(imageData,0,0);
-
-          };
-
+            ctxn.putImageData(imageData,0,0);
+            }
           function setSol(imageData, x, y){
             var index=(x+y*imageData.width) * 4;
 
@@ -137,15 +152,16 @@ var imageData;
             if(imageData.data[index+2]>127){imageData.data[index+2]=255-imageData.data[index+2];}
           };
 
-          function solarizar(imageData, x, y){
-            var imageData = ctx.getImageData(0, 0, image1.width, image1.height);
+          function solarizar(ctxo,ctxn){
+          var imageData = ctxo.getImageData(0, 0, canvaswidth, canvasheight);
+
 
             for (var i = 0; i < imageData.width; i++) {
               for (var j = 0; j < imageData.height; j++) {
                 setSol(imageData, i, j);
               }
             }
-            ctx3.putImageData(imageData,0,0,(image1.width)/2,(image1.height)/2);
+            ctxn.putImageData(imageData,0,0);
 
           };
 
@@ -157,15 +173,16 @@ var imageData;
             imageData.data[index+2]=255-imageData.data[index+2];
           };
 
-          function invertir(){
-            var imageData = ctx.getImageData(0, 0, image1.width, image1.height);
+          function invertir(ctxo,ctxn){
+          var imageData = ctxo.getImageData(0, 0, canvaswidth, canvasheight);
+
 
             for (var i = 0; i < imageData.width; i++) {
               for (var j = 0; j < imageData.height; j++) {
                 setInv(imageData, i, j);
               }
             }
-            ctx5.putImageData(imageData,0,0);
+            ctxn.putImageData(imageData,0,0);
 
           };
 
@@ -174,22 +191,23 @@ var imageData;
             red = imageData.data[index+0]
             green = imageData.data[index+1]
             blue = imageData.data[index+2]
-          
+
             imageData.data[index+0] = (red * 0.393) + (green * 0.769) + (blue * 0.189);
             imageData.data[index+1] = (red * 0.349) + (green * 0.686) + (blue * 0.168);
             imageData.data[index+2] = (red * 0.272) + (green * 0.534) + (blue * 0.131);
           };
-      
 
-          function sepia(){
-            var imageData = ctx2.getImageData(0, 0, image1.width, image1.height);
+
+          function sepia(ctxo,ctxn){
+          var imageData = ctxo.getImageData(0, 0, canvaswidth, canvasheight);
+
 
             for (var i = 0; i < imageData.width; i++) {
               for (var j = 0; j < imageData.height; j++) {
                 setSepia(imageData, i, j);
               }
             }
-            ctx4.putImageData(imageData,0,0);
+            ctxn.putImageData(imageData,0,0);
 
           };
 
@@ -202,19 +220,20 @@ var imageData;
             imageData.data[index+2]=prom;
           };
 
-          function blancoYnegro(){
-            var imageData = ctx.getImageData(0, 0, image1.width, image1.height);
+          function blancoYnegro(ctxo,ctxn){
+          var imageData = ctxo.getImageData(0, 0, canvaswidth, canvasheight);
+
 
             for (var i = 0; i < imageData.width; i++) {
               for (var j = 0; j < imageData.height; j++) {
                 setProm(imageData, i, j);
               }
             }
-            ctx3.putImageData(imageData,0,0);
+            ctxn.putImageData(imageData,0,0);
 
           };
-          function espejo(){
-            var imageData = ctx.getImageData(0, 0, image1.width, image1.height);
+          function espejo(ctxo,ctxn){
+            var imageData = ctxo.getImageData(0, 0, image1.width, image1.height);
                 for(var i = 0; i < image1.height; i++) {
                   for(var j = 0; j < image1.width; j++) {
                     var off = (i * image1.width + j) * 4;
@@ -225,143 +244,25 @@ var imageData;
                     imageData.data[dstOff+3] = imageData.data[off+3];
                   }
                 }
-                ctx8.putImageData(imageData,0,0);
+                ctxn.putImageData(imageData,0,0);
               };
-        function sharpear(){
+        function sharpear(ctxo,ctxn){
           var matriz=
           [[  0, -1,  0],
           [-1,  5, -1],
           [ 0, -1,  0 ]]
-          var imageData = ctx.getImageData(0, 0, image1.width, image1.height);
-          setkernel(imageData,matriz,ctx7);
+          var imageData = ctxo.getImageData(0, 0, canvaswidth, canvasheight);
+          setkernel(imageData,matriz,ctxn);
         }
-        function blurear(){
+        function blurear(ctxo,ctxn){
           var matriz=
           [[  1/9, 1/9, 1/9],
           [1/9, 1/9, 1/9],
           [ 1/9, 1/9,  1/9 ]]
-          var imageData = ctx.getImageData(0, 0, image1.width, image1.height);
-          setkernel(imageData,matriz,ctx8);
+          var imageData = ctxo.getImageData(0, 0, canvaswidth, canvasheight);
+          setkernel(imageData,matriz,ctxn);
         }
-        
 
-        //function cosarara(){
-//
-        //var sobel_x =
-        //  [[0,-1,0],
-        //   [-1,5,-1],
-        //   [0,-1,0]]
-        //var sobel_y=
-        //  [[0,-1,0],
-        //   [-1,5,-1],
-        //   [0,-1,0]]
-        //   var imageData = ctx2.getImageData(0, 0, image1.width, image1.height);
-        //   //for (var i = 0; i < imageData.width; i++) {
-        //   //  for (var j = 0; j < imageData.height; j++) {
-        //   //    setProm(imageData, i, j);
-        //   //  }
-        //   //}
-        //   //ctx6.putImageData(imageData,0,0);
-        //   var imageData2 =imageData;
-        //       for(var i = 0; i < image1.height; i++) {
-        //         for(var j = 0; j < image1.width; j++) {
-        //              setcosarara(imageData2,imageData,i,j,sobel_x,sobel_y)
-        //          }
-        //        }
-        //      ctx6.putImageData(imageData2,0,0);
-        //}
-      //  function setcosarara(imageData2,imageData,x,y,f,g){
-      //    var i = (y + x * imageData.width)*4
-      //    var xr=0;
-      //    for(var a = 0; a < 3; a++)
-      //    {
-      //        for(var b = 0; b < 3; b++)
-      //        {
-      //            var xn = x + a - 1;
-      //            var yn = y + b - 1;
-//
-      //            var index = (yn + xn * imageData.width)*4;
-      //            xr=  (imageData.data[index] * f[a][b])+xr;
-      //        }
-      //    }
-      //    var yr=0;
-      //    for(var a = 0; a < 3; a++)
-      //    {
-      //        for(var b = 0; b < 3; b++)
-      //        {
-      //            var xn = x + a - 1;
-      //            var yn = y + b - 1;
-//
-      //            var index = (yn + xn * imageData.width)*4;
-      //            yr=  (imageData.data[index] * g[a][b])+yr;
-      //        }
-      //    }
-      //    var xg=0;
-      //    for(var a = 0; a < 3; a++)
-//
-      //    {
-      //        for(var b = 0; b < 3; b++)
-      //        {
-      //            var xn = x + a - 1;
-      //            var yn = y + b - 1;
-//
-      //            var index = (yn + xn * imageData.width)*4;
-      //            xg =  (imageData.data[index+1] * f[a][b])+xg;
-      //        }
-      //    }
-      //    var yg=0;
-      //    for(var a = 0; a < 3; a++)
-//
-      //    {
-      //        for(var b = 0; b < 3; b++)
-      //        {
-      //            var xn = x + a - 1;
-      //            var yn = y + b - 1;
-//
-      //            var index = (yn + xn * imageData.width)*4;
-      //            yg =  (imageData.data[index+1] * g[a][b])+yg;
-      //        }
-      //    }
-      //    var xb=0;
-      //    for(var a = 0; a < 3; a++)
-//
-      //    {
-      //        for(var b = 0; b < 3; b++)
-      //        {
-      //            var xn = x + a - 1;
-      //            var yn = y + b - 1;
-//
-      //            var index = (yn + xn * imageData.width)*4;
-      //            xb =  (imageData.data[index+2] * f[a][b])+xb;
-      //        }
-      //    }
-      //    var yb=0;
-      //    for(var a = 0; a < 3; a++)
-//
-      //    {
-      //        for(var b = 0; b < 3; b++)
-      //        {
-      //            var xn = x + a - 1;
-      //            var yn = y + b - 1;
-//
-      //            var index = (yn + xn * imageData.width)*4;
-      //            yb = (imageData.data[index+2] * g[a][b])+yb;
-      //        }
-      //    }
-      //    xr=Math.abs(xr);
-      //    yr=Math.abs(yr);
-      //    xg=Math.abs(xg);
-      //    yg=Math.abs(yg);
-      //    xb=Math.abs(xb);
-      //    yb=Math.abs(yb);
-      //    v = Math.sqrt((xr*xr)+(yr*yr));
-      //    h = Math.sqrt((xg*xg)+(yg*yg));
-      //    z = Math.sqrt((xb*xb)+(yb*yb));
-      //     imageData2.data[i+0]=xr;
-      //     imageData2.data[i+1]=xg;
-      //      imageData2.data[i+2]=xb;
-      //     imageData2.data[i+3]=255;
-      //}
       function setkernel(imageData,f,ctx){
           var src = imageData.data;
           var sw = imageData.width;
